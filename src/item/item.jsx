1b7c769132ceba0cc02bb681b99c6da20760250c
ridapr <ridapr@byu.edu>
2024-11-11 21:09:04 -0700

    Created css for profile and item pages

--- src/item/item.jsx
+++ src/item/item.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 
+import './item.css';
+
 export function Item(){
     return (
         <main>
             <h1>"Title"</h1>
-                <div>
+                <div className="thumbnail-container">
                     <img src="thumbnail_placeholder.png" alt="thumbnail" width="200" height="300"/>
                 </div>
-                <div>Release Date: </div>
-                <div>Description: </div>
-                <div>
+                <div className='info-section'>Release Date: </div>
+                <div className='info-section'>Description: </div>
+                <div className='input-group'>
                     <span>Rating:</span>
                     <input type="number" placeholder="Rating" />
                 </div>
-                <div>
+                <div className='input-group'>
                     <span>Review:</span>
                     <input type="text" placeholder="Review" />
                 </div>
                 <button type="submit">Submit</button>
-                <div>
+                <div className='average-rating'>
                     <span>Average Rating: ***** (this will use websocket data to show the average rating of the media)</span>
                 </div>
                 <h2>Reviews (Websocket data)</h2>
-                <div>
+                <div className='review-item'>
                     <span>Review by User123: (Placeholder)</span>
                 </div>
         </main>
--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './profile.css'
+
 export function Profile() {
     return (
         <main>
@@ -8,23 +10,23 @@ export function Profile() {
             <h1>Profile Name Placeholder</h1>
             <p>Placeholder for Profile info/stats</p>
             <h2>Recent Reviews</h2>
-            <ul class="recent-reviews">
-            <li class="review-box">
-                <div class="review-title">Review Title 1</div>
-                <div class="review-rating">Rating: 4.5 stars</div>
-            </li>
-            <li class="review-box">
-                <div class="review-title">Review Title 2</div>
-                <div class="review-rating">Rating: 3.0 stars</div>
-            </li>
-            <li class="review-box">
-                <div class="review-title">Review Title 3</div>
-                <div class="review-rating">Rating: 5.0 stars</div>
-            </li>
-            <li class="review-box">
-                <div class="review-title">Review Title 4</div>
-                <div class="review-rating">Rating: 2.5 stars</div>
-            </li>
+            <ul className="recent-reviews">
+                <li className="review-box">
+                    <div className="review-title">Review Title 1</div>
+                    <div className="review-rating">Rating: 4.5 stars</div>
+                </li>
+                <li className="review-box">
+                    <div className="review-title">Review Title 2</div>
+                    <div className="review-rating">Rating: 3.0 stars</div>
+                </li>
+                <li className="review-box">
+                    <div className="review-title">Review Title 3</div>
+                    <div className="review-rating">Rating: 5.0 stars</div>
+                </li>
+                <li className="review-box">
+                    <div className="review-title">Review Title 4</div>
+                    <div className="review-rating">Rating: 2.5 stars</div>
+                </li>
             </ul>
         </main>
     );
